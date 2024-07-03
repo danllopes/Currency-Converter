7a152ccb5d5920a5e7878c1128655669a76ea8a9
“Daniel <danielanderson.lopes0@gmail.com>
2024-07-03 16:42:21 -0300

    feat: adiciona resolução do exercício

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import util.CurrencyConverter;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Locale.setDefault(Locale.US);
+        var sc = new Scanner(System.in);
+
+        System.out.print("\n\nWhat is the dollar price? ");
+        double price = sc.nextDouble();
+
+        System.out.print("\nHow manu dollars will be bought: ");
+        double amount = sc.nextDouble();
+
+        System.out.printf("\nAmount to be paid in reais =  %.2f\n\n", CurrencyConverter.converter(price, amount));
+
+        sc.close();
     }
 }
